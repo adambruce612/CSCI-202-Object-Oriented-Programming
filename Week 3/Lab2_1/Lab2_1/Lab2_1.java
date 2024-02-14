08f9d757444e6065a5b91a3fcb64cc0573f6b4e8
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2024-02-13 19:48:25 -0600

    Add files via upload

---
+++ Week 3/Lab2_1/Lab2_1/Lab2_1.java
@@ -0,0 +1,63 @@
+package lab2_1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+
+public class Lab2_1 extends Application
+{
+    @Override
+    public void start(Stage stage)
+    {
+        // TODO: Add code here to draw the house
+        Group group = new Group();
+
+        // Create the walls of the house
+        Rectangle walls = new Rectangle(0, 100, 200, 150);
+        walls.setFill(Color.RED);
+        group.getChildren().add(walls);
+
+        // Create the door of the house
+        Rectangle door = new Rectangle(25, 150,55, 150);
+        door.setFill(Color.web("rgb(109, 89, 89)"));
+        group.getChildren().add(door);
+
+        // Create window
+        Rectangle window = new Rectangle(125, 150, 50, 50);
+        window.setFill(Color.web("hsl(157, 62%, 80%)"));
+        window.setStroke(Color.BLACK);
+        group.getChildren().add(window);
+
+        // Create roof
+        Polygon roof = new Polygon();
+        roof.setFill(Color.web("#974E00")); // roof color is brown
+        roof.getPoints().addAll(0.0, 100.0);
+        roof.getPoints().addAll(100.0, 0.0);
+        roof.getPoints().addAll(200.0, 100.0);
+        group.getChildren().add(roof);
+
+        // Create the sun
+        Circle sun = new Circle(0,0,40); // center (0,0), radius 40
+        sun.setFill(Color.YELLOW);
+        group.getChildren().add(sun);
+
+        Pane mainPane = new Pane(group);
+        mainPane.setStyle("-fx-background-color: lightblue");
+
+        Scene scene = new Scene(mainPane, 200, 250);
+        stage.setTitle("Lab2_1");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    // The main() method is only needed for IDEs with limited JavaFX support
+    public static void main(String[] args)
+    {
+        Application.launch(args);
+    }
+}
